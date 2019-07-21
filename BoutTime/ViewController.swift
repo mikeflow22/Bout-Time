12e56d7bf6d73575497408528dd0afd234aa8acc
Michael R.F <mrflowers22@uwalumni.com>
2019-07-21 16:01:18 -0700

    first commit via terminal

---
+++ BoutTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoutTime
+//
+//  Created by Michael Flowers on 7/21/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

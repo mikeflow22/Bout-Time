4d64cecb38591b289098a634afd615388b05b04b
Michael R.F <mrflowers22@uwalumni.com>
2019-08-05 16:20:58 -0700

    added model and model controller file.

---
+++ BoutTime/EventController/EventController.swift
@@ -0,0 +1,14 @@
+//
+//  EventController.swift
+//  BoutTime
+//
+//  Created by Michael Flowers on 8/5/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class EventController {
+    var events: [Event] = []
+    
+}
---
+++ BoutTime/Model/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  BoutTime
+//
+//  Created by Michael Flowers on 8/5/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Event: Equatable {
+    var question: String
+    var year: Int
+}

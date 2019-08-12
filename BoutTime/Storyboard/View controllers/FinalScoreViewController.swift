f7503702db1ee8bc67376f9767e9596cf8204119
Michael R.F <mrflowers22@uwalumni.com>
2019-08-12 09:47:36 -0700

    added finalScoreViewController two labels and a button.

---
+++ BoutTime/Storyboard/View controllers/FinalScoreViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FinalScoreViewController.swift
+//  BoutTime
+//
+//  Created by Michael Flowers on 8/12/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import UIKit
+
+class FinalScoreViewController: UIViewController {
+
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var playAgainProperties: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+}
